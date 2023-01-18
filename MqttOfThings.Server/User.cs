ab24d2fbbe16cf407ccfb1ba80e5ae730b06c395
ACACUSGROUP\b.hejazi <bqumsan99@gmail.com>
2023-01-18 14:03:23 +0300

    Refactoring

--- MqttOfThings.Server/User.cs
+++ MqttOfThings.Server/User.cs
@@ -2,25 +2,14 @@ namespace MqttOfThings;
 
 public class User
 {
-    public string UserName { get; set; } = string.Empty;
+    public string UserName { get; init; } = string.Empty;
 
-    public string ClientId { get; set; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
 
-    public string Password { get; set; } = string.Empty;
-
-    public TopicTuple SubscriptionTopicLists { get; set; } = new();
-
-    public TopicTuple PublishTopicLists { get; set; } = new();
-}
-
-public class TopicTuple
-{
-    public List<string> WhitelistTopics { get; set; } = new();
-
-    public List<string> BlacklistTopics { get; set; } = new();
+    public string ClientId { get; init; } = string.Empty;
 }
 
 public class Config
 {
-    public List<User> Users { get; set; } = new();
+    public List<User> Users = null!;
 }
